a3c8cf0f907101c3c09ac75e21ce0bb0acf6a546
Rony Lev Ari <ronylevari@gmail.com>
2016-07-04 02:29:08 +0300

    Added no_content fragment and intitial navigation content functionality

--- app/src/main/java/com/example/ronylevari/callrecorder/MainActivity.java
+++ app/src/main/java/com/example/ronylevari/callrecorder/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.ronylevari.callrecorder;
 
+
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,12 +16,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
+
+import com.example.ronylevari.callrecorder.constants.AppConstants;
+import com.example.ronylevari.callrecorder.fragments.FragmentNoContent;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    public static String TAG = "Main Activity";
     private MenuItem mActiveMenuItem;
+    private int mActiveMenuItemId;
+    private boolean mFirstUpload;
+    private boolean mIsContentAvailable;
+    private Toolbar mToolbar;
+    private FloatingActionButton mFab;
+    private DrawerLayout mDrawer;
+    private NavigationView mNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +39,13 @@ public class MainActivity extends AppCompatActivity
 
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        // set up UI toolbar
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        // initialize fab onclick listener
+        mFab = (FloatingActionButton) findViewById(R.id.fab);
+        mFab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -37,14 +53,71 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        // set up the hamburger icon to open and close the drawer
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
+                this, mDrawer, mToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        // listen for navigation events
+        mNavigationView = (NavigationView) findViewById(R.id.nav_view);
+        mNavigationView.setNavigationItemSelectedListener(this);
+
+        // load saved navigation state if present
+        if (savedInstanceState != null) {
+            mActiveMenuItemId = savedInstanceState.getInt(AppConstants.SAVE_NAV_MENU_OPTION);
+        } else {
+            mActiveMenuItemId = R.id.nav_recordings;
+        }
+        mNavigationView.getMenu().findItem(mActiveMenuItemId).setChecked(true);
+
+        // load content according to navigation state
+        displayMainContent(mActiveMenuItemId);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        // save navigation menu id to initialize the right content
+        outState.putInt(AppConstants.SAVE_NAV_MENU_OPTION, mActiveMenuItemId);
+        Log.d(TAG, "active navigation item saved");
     }
 
     @Override
@@ -90,29 +163,84 @@ public class MainActivity extends AppCompatActivity
             mActiveMenuItem.setChecked(false);
         mActiveMenuItem = item;
         item.setChecked(true);
+
         // Handle navigation view item clicks here.
-        int id = item.getItemId();
+        mActiveMenuItemId = item.getItemId();
 
-       if (id == R.id.nav_Recordings) {
-            Toast toast = Toast.makeText(this, "messages", Toast.LENGTH_SHORT);
-            toast.show();
-        } else if (id == R.id.nav_messages) {
-            Toast toast = Toast.makeText(this, "archive", Toast.LENGTH_SHORT);
-            toast.show();
-        } else if (id == R.id.nav_archive) {
-            Toast toast = Toast.makeText(this, "trash", Toast.LENGTH_SHORT);
-            toast.show();
-        } else if (id == R.id.nav_trash) {
-           Toast toast = Toast.makeText(this, "help", Toast.LENGTH_SHORT);
-           toast.show();
-       } else if (id == R.id.nav_help) {
-           Toast toast = Toast.makeText(this, "help", Toast.LENGTH_SHORT);
-           toast.show();
+        displayMainContent(mActiveMenuItemId);
 
+//       if (mActiveMenuItemId == R.id.nav_recordings) {
+//            Toast toast = Toast.makeText(this, "messages", Toast.LENGTH_SHORT);
+//            toast.show();
+//        } else if (mActiveMenuItemId == R.id.nav_messages) {
+//            Toast toast = Toast.makeText(this, "archive", Toast.LENGTH_SHORT);
+//            toast.show();
+//        } else if (mActiveMenuItemId == R.id.nav_archive) {
+//            Toast toast = Toast.makeText(this, "trash", Toast.LENGTH_SHORT);
+//            toast.show();
+//        } else if (mActiveMenuItemId == R.id.nav_trash) {
+//           Toast toast = Toast.makeText(this, "help", Toast.LENGTH_SHORT);
+//           toast.show();
+//       } else if (mActiveMenuItemId == R.id.nav_help) {
+//           Toast toast = Toast.makeText(this, "help", Toast.LENGTH_SHORT);
+//           toast.show();
+//        }
+//
+//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    public void displayMainContent(int menuItemId) {
+
+        Fragment fragment = null;
+        String title = "";
+        Bundle bundle = new Bundle();
+        String message = "";
+
+        switch (menuItemId) {
+            case R.id.nav_recordings:
+                message = "You have no new recordings";
+                bundle.putString(AppConstants.NO_CONTENT_TITLE, "You have no new recordings");
+                title  = "Recordings";
+                break;
+            case R.id.nav_messages:
+                message = "You have no new recordings";
+                bundle.putString(AppConstants.NO_CONTENT_TITLE, "You have no new recordings");
+                title = "Default Return message";
+                break;
+            case R.id.nav_archive:
+                message = "You have no archived items";
+                bundle.putString(AppConstants.NO_CONTENT_TITLE, "You have no archived items");
+                title  = "Archive";
+                break;
+            case R.id.nav_trash:
+                message = "Trash is empty";
+                bundle.putString(AppConstants.NO_CONTENT_TITLE, "Trash is empty");
+                title = "Trash";
+                break;
+            default:
+                message = "You have no new recordings";
+                bundle.putString(AppConstants.NO_CONTENT_TITLE, "You have no new recordings");
+                title = "Recordings";
+                break;
+        }
+
+        fragment = FragmentNoContent.newInstance(message);
+
+        if (fragment != null) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.replace(R.id.content_frame, fragment);
+            ft.commit();
+        }
+
+        // set the toolbar title
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(title);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
+
 }
---
+++ app/src/main/java/com/example/ronylevari/callrecorder/constants/AppConstants.java
@@ -0,0 +1,8 @@
+package com.example.ronylevari.callrecorder.constants;
+
+public class AppConstants {
+
+    public static final String SAVE_NAV_MENU_OPTION = "save navigation menu option";
+    public static final String NO_CONTENT_TITLE = "no content text";
+    public static final String NO_CONTENT = "No content to display";
+}
